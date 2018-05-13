93f040367322558aaec0777a644954e833944a55
Paper <paperhs901230@gmail.com>
2018-05-13 14:34:09 +0800

    init my config

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -21,19 +21,18 @@
   (add-to-list 'package-archives (cons "melpa" (concat proto "://elpa.emacs-china.org/melpa/")) t)
   ;; Official MELPA Mirror, in case necessary.
   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
-
-  (if (< emacs-major-version 24)
+    (if (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.emacs-china.org/gnu/")))
     (unless no-ssl
       ;; Force SSL for GNU ELPA
       (setcdr (assoc "gnu" package-archives) "http://elpa.emacs-china.org/gnu/"))))
 
+)
 
 ;; We include the org repository for completeness, but don't normally
 ;; use it.
 (add-to-list 'package-archives '("org" . "http://elpa.emacs-china.org/org/"))
-
 
 
 
