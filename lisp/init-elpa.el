ac2febfdf0bc1492c522247da2577fef47108afa
Steve Purcell <steve@sanityinc.com>
2019-09-13 13:34:34 +1200

    Don't override the default protocol for ELPA

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -21,6 +21,7 @@
   (add-to-list 'package-archives (cons "melpa" (concat proto "://elpa.emacs-china.org/melpa/")) t)
   ;; Official MELPA Mirror, in case necessary.
   ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
+
   (if (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.emacs-china.org/gnu/")))
@@ -32,6 +33,8 @@
 ;; We include the org repository for completeness, but don't normally
 ;; use it.
 (add-to-list 'package-archives '("org" . "http://elpa.emacs-china.org/org/"))
+
+
 
 
 
