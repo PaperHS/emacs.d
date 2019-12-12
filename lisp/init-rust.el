a29f1709d3724a4561d752bf254d5a27ad8b61d6
PaperHS <paperhs901230@gmail.com>
2019-12-12 09:51:59 +0800

    add awesome eshell

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -2,15 +2,47 @@
 ;;; Commentary:
 ;;; Code:
 
-(when (maybe-require-package 'rust-mode)
-  (when (maybe-require-package 'racer)
-    (add-hook 'rust-mode-hook #'racer-mode))
-  (when (maybe-require-package 'company)
-    (add-hook 'racer-mode-hook #'company-mode)))
+;; (when (maybe-require-package 'rust-mode)
+;;   (when (maybe-require-package 'racer)
+;;     (add-hook 'rust-mode-hook #'racer-mode))
+;;   (when (maybe-require-package 'company)
+;;     (add-hook 'racer-mode-hook #'company-mode)))
 
-(when (maybe-require-package 'flycheck-rust)
-  (after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+;; (when (maybe-require-package 'flycheck-rust)
+;;   (after-load 'rust-mode
+;;     (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
+
+(use-package company
+  :hook (prog-mode . company-mode)
+  :config (setq company-tooltip-align-annotations t)
+  (setq company-minimum-prefix-length 1))
+
+(use-package lsp-mode
+  :hook (rust-mode . lsp)
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
+
+
+
+
+
+(use-package toml-mode)
+
+(use-package rust-mode
+  :hook (rust-mode . lsp))
+
+;; ;; Add keybindings for interacting with Cargo
+;; (use-package cargo
+;;   :hook (rust-mode . cargo-minor-mode))
+
+(use-package flycheck-rust
+  :config (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 
 (provide 'init-rust)
